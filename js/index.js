93a7f8f2de3c17b399d8e9e15a262ad6fd13c1be
cra123 <Onixmohamed@yahoo.fr>
2022-07-30 22:49:20 +0300

    Frontend Changes

--- js/index.js
+++ js/index.js
@@ -3,42 +3,67 @@ window.onload = () => {
     startGame();
   };
 
+  document.getElementById('restart-button').onclick = () => {
+    restartGame();
+  };
+
+  //Reload window and Clear Cache
+
+  function restartGame(){
+    window.location.reload();
+  }
+
+
+
   //creating a new image
-  const road = new Image();
-  road.src = 'images/road.png';
-  const car = new Image();
-  car.src = 'images/car.png';
+  const ball = new Image();
+  ball.src = 'images/throw-ball.png';
+
+
+  //creating girl on the left
+  const girlLeft = new Image();
+  girlLeft.src = 'images/girl-left.png';
+  
+  //Creating girl on the right
+  const girlRight = new Image();
+  girlRight.src = 'images/girl-right.png';
 
 
   function startGame() {
-  //Draw the road 
+
+  const startbutton = document.getElementById('start-button');
+  const resetbutton = document.getElementById('restart-button');
+  const pausebutton = document.getElementById('pause-button');
+  const endbutton = document.getElementById('end-button');
+  const gameArea = document.getElementById('game-board');
+
+  startbutton.disabled = true;
+  startbutton.style.backgroundColor = 'skyblue';
+  resetbutton.disabled = false;
+  resetbutton.style.backgroundColor = 'green';
+  endbutton.disabled = false;
+  endbutton.style.backgroundColor = 'red';
+  gameArea.style.display = "flex";
+
+  //Change div display to none
+  const instructionDiv = document.getElementsByClassName('instruction');
+  instructionDiv[0].style.display = 'none';
+
+
+  
+  
 
   const theCanvas = document.getElementById('canvas');
   const ctx = theCanvas.getContext('2d');
 
-  ctx.drawImage(road, 0, 0, theCanvas.width, theCanvas.height);
+  //Draw the ball on the screen
+  ctx.drawImage(ball, theCanvas.width/2, theCanvas.height/2, 50, 50);
 
-  //Draw the car
-  ctx.drawImage(car, theCanvas.width/2, 500, 50, 70);
+  //Draw the girl-left on the screen
+  ctx.drawImage(girlRight, theCanvas.width/4, theCanvas.height/2, 50, 50)
 
-    //Move car left 
-    document.addEventListener('keydown', (event) => {
-      if (event.keyCode === 37) {
-        ctx.clearRect(0, 0, theCanvas.width, theCanvas.height);
-        ctx.drawImage(road, 0, 0, theCanvas.width, theCanvas.height);
-        ctx.drawImage(car, theCanvas.width/2 - 50, 500, 50, 70);
-      }
-    }
-
-
-    //Move car right
-    document.addEventListener('keydown', (event) => {
-      if (event.keyCode === 39) {
-        ctx.clearRect(0, 0, theCanvas.width, theCanvas.height);
-        ctx.drawImage(road, 0, 0, theCanvas.width, theCanvas.height);
-        ctx.drawImage(car, theCanvas.width/2 + 50, 500, 50, 70);
-      }
-    }
+  //Draw the girl-left on the screen
+  ctx.drawImage(girlLeft, theCanvas.width/2, theCanvas.height/4, 50, 50)
 
 
       
